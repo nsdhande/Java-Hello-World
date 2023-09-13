762e604820cf576afbd292842ef19817ec074f2d
nsdhande <144787233+nsdhande@users.noreply.github.com>
2023-09-13 14:52:48 +0530

    Update main.java
    Updated changes

--- main.java
+++ main.java
@@ -8,5 +8,6 @@ class main {
     // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
+    System.out.println("This is new added line please watch it will rebuild again automatically");
   }
 }
